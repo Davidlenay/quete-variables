d5b3c4079f2b7b0050d1c633159b7e4b03fb88c8
DAVIDLNAY <davidlenay@gmail.com>
2023-04-11 15:57:57 +0200

    first commit

---
+++ Indiana.java
@@ -0,0 +1,12 @@
+public class Indiana {
+
+    public static void main(String[] args) {
+
+        String name = "Indiana Jones and the Last Crusade";
+        boolean seen = true;
+        int years = 1989;
+        double notation = 8.2;
+
+        System.out.println(name + "\n" +  seen + "\n" +  years + "\n" + notation);        
+    }
+}
